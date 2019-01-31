0c134fa4931dbb801c77f9342eb4d691452e37ec
blake <blake@ganguo.hk>
2019-01-31 15:43:00 +0800

    Initial commit

---
+++ src/main/java/com/example/junitest/JunitestApplication.java
@@ -0,0 +1,14 @@
+package com.example.junitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitestApplication.class, args);
+    }
+
+}
+
